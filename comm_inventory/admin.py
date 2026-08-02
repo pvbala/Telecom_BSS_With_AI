dabbae96f91086d33022fae367c70e1487b4d8e9
pvbala <pvbalaji.varadhachary@gmail.com>
2026-08-02 18:21:28 +0530

    Delete comm_inventory directory

--- comm_inventory/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import StockItem
-
-
-@admin.register(StockItem)
-class StockItemAdmin(admin.ModelAdmin):
-    list_display = ("product", "item_type", "batch_reference", "quantity_on_hand", "quantity_reserved", "quantity_available")
-    list_filter = ("item_type", "product")
