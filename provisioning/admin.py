0152ae87e88df5bc0bfcc1913332c63acf260517
pvbala <pvbalaji.varadhachary@gmail.com>
2026-08-02 18:23:55 +0530

    Delete provisioning directory

--- provisioning/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ProvisioningJob
-
-
-@admin.register(ProvisioningJob)
-class ProvisioningJobAdmin(admin.ModelAdmin):
-    list_display = ("order", "status", "resource_identifier", "created_at")
-    list_filter = ("status",)
