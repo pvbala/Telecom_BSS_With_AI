98bb2f02dc2af22a033610c32ebb6cb35cdb7bde
pvbala <pvbalaji.varadhachary@gmail.com>
2026-08-02 18:24:56 +0530

    Delete tickets directory

--- tickets/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Ticket
-
-
-@admin.register(Ticket)
-class TicketAdmin(admin.ModelAdmin):
-    list_display = ("id", "customer", "subject", "priority", "status", "created_at")
-    list_filter = ("status", "priority")
-    search_fields = ("customer__name", "subject")
