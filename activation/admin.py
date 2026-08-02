be5218372e7184186737c20a61348238310e4515
pvbala <pvbalaji.varadhachary@gmail.com>
2026-08-02 18:20:00 +0530

    Delete activation directory

--- activation/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ActivationLog
-
-
-@admin.register(ActivationLog)
-class ActivationLogAdmin(admin.ModelAdmin):
-    list_display = ("order", "status", "created_at")
-    list_filter = ("status",)
--- activation/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ActivationConfig(AppConfig):
-    name = 'activation'
