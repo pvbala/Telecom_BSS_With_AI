c5275afcb63813df8e27ac98349a2c1658fc5265
pvbala <pvbalaji.varadhachary@gmail.com>
2026-08-02 18:24:09 +0530

    Delete resource_inventory directory

--- resource_inventory/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import NetworkResource
-
-
-@admin.register(NetworkResource)
-class NetworkResourceAdmin(admin.ModelAdmin):
-    list_display = ("identifier", "resource_type", "status", "assigned_to_order", "updated_at")
-    list_filter = ("resource_type", "status")
-    search_fields = ("identifier",)
--- resource_inventory/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ResourceInventoryConfig(AppConfig):
-    name = 'resource_inventory'
