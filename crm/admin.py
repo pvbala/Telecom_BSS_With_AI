91f40d4bed9aaeafc7e1571ac2cb9993f58180b7
pvbala <pvbalaji.varadhachary@gmail.com>
2026-08-02 18:22:20 +0530

    Delete crm directory

--- crm/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Interaction
-
-
-@admin.register(Interaction)
-class InteractionAdmin(admin.ModelAdmin):
-    list_display = ("customer", "channel", "summary", "created_at")
-    list_filter = ("channel",)
-    search_fields = ("customer__name", "summary")
