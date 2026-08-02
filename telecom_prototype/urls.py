f0193a5027422277ad3a01fe924d6c67ecc4ccc2
pvbala <pvbalaji.varadhachary@gmail.com>
2026-08-02 18:24:28 +0530

    Delete telecom_prototype directory

--- telecom_prototype/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from assurance.views import dashboard
-
-urlpatterns = [
-    path("admin/assurance/dashboard/", dashboard, name="assurance-dashboard"),
-    path("admin/", admin.site.urls),
-]
